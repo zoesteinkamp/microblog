402634786b5902ffab4daf9403d5526d14d0a93b
Zoe Steinkamp <zoe.steinkamp@gmail.com>
2015-11-04 21:49:34 -0700

    Part 5
    User auth and login

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,17 @@
 from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
+import os
+from flask.ext.login import LoginManager
+from flask.ext.openid import OpenID
+from config import basedir
+
 
 app = Flask(__name__)
 app.config.from_object('config')
 db = SQLAlchemy(app)
 
+lm = LoginManager()
+lm.init_app(app)
+lm.login_view = 'login'
 
 from app import views, models
--- app/models.py
+++ app/models.py
@@ -4,6 +4,25 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     nickname = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
+    posts = db.relationship('Post', backref='author', lazy='dynamic')
+
+    @property
+    def is_authenticated(self):
+        return True
+
+    @property
+    def is_active(self):
+        return True
+
+    @property
+    def is_anonymous(self):
+        return False
+
+    def get_id(self):
+        try:
+            return unicode(self.id)  # python 2
+        except NameError:
+            return str(self.id)  # python 3
 
     def __repr__(self):
         return '<User %r>' % (self.nickname)
